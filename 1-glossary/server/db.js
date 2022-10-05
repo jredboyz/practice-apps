f5f57cd62d2fe0196f5880eb430bcbdc272fe1da
Joseph <jredboyz@gmail.com>
2022-10-05 14:41:05 -0700

    able to send request and get data from db

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,26 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1/glossaryApp')
+.catch((error => console.log('error connecting to database')))
+
+const glossarySchema = new mongoose.Schema({
+  word: String,
+  defintion: String
+})
+
+const Glossary = mongoose.model('Glossary', glossarySchema)
+
+const getWords = (cb) => {
+  Glossary.find({}).exec((err, results) => {
+    if (err) {
+      console.log(err, 'ERROR in getWords')
+    }
+    cb(null, results)})
+}
+
+module.exports = {
+  getWords
+}
+
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
+const {getWords} = require("./db.js")
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.get('/glossary', (req, res) => {
+  getWords((err, result) => {
+    if (err) {
+      console.log(err, 'ERROR getting words')
+    }
+    res.send(result);
+  })
+})
+
+app.post('/glossary', (req, res) => {
+  // addWord()
+  res.status(201).send('POST')
+})
+
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
